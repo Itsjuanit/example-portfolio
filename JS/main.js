0882d4e4d271b73bf505d9c22c454c457cc10af5
Juan Ignacio Tejada <tejadajuanignacio12@gmail.com>
2020-08-22 01:58:01 -0300

    Dark Mode

--- JS/main.js
+++ JS/main.js
@@ -1,15 +1,12 @@
-import Darkmode from 'darkmode-js';
-
-
 const options = {
-  bottom: '64px', // default: '32px'
+  bottom: '15px', // default: '32px'
   right: 'unset', // default: '32px'
   left: '32px', // default: 'unset'
   time: '0.5s', // default: '0.3s'
   mixColor: '#fff', // default: '#fff'
   backgroundColor: '#fff',  // default: '#fff'
   buttonColorDark: '#100f2c',  // default: '#100f2c'
-  buttonColorLight: '#fafafa', // default: '#fff'
+  buttonColorLight: '#fff', // default: '#fff'
   saveInCookies: false, // default: true,
   label: '🌓', // default: ''
   autoMatchOsTheme: true // default: true
