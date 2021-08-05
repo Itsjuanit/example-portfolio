3d4814af4b8e29200ec8ddd9cf347122a449598d
Juan Ignacio Tejada <tejadajuanignacio12mail.com>
2021-08-05 13:57:29 -0300

    Cambios en el copy

--- JS/index.js
+++ JS/index.js
@@ -1,6 +1,8 @@
 const mybutton = document.getElementById("myBtn");
 
-window.onscroll = function() {scrollFunction()};
+window.onscroll = function () {
+  scrollFunction();
+};
 
 function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
@@ -9,7 +11,6 @@ function scrollFunction() {
     mybutton.style.display = "none";
   }
 }
-
 
 function topFunction() {
   document.body.scrollTop = 0;
