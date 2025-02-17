b6c56b759dba449610df83ef3ee83e7543b0cd79
Juan Ignacio Tejada <tejadajuanignacio12@gmail.com>
2025-02-17 09:37:50 -0300

    Se agregan cambios

--- SRC/projects.js
+++ SRC/projects.js
@@ -8,61 +8,72 @@ const courflixImage = "./SRC/assets/netflix.png";
 const projectsData = [
   {
     id: 0,
+    title: "PAGATODOYA",
+    opinion:
+      "Una webapp intuitiva para gestionar tus gastos diarios de forma eficiente. Agrega, edita y elimina movimientos, y obtén reportes detallados de tu actividad financiera mensual.",
+    tag: "REACT,CSS,JAVASCRIPT,FIREBASE,PRIMEREACT,VERCEL",
+    url: "https://budget-app-gilt-phi.vercel.app/login",
+    img: cryptoImage,
+  },
+  {
+    id: 1,
     title: "Crypto App",
     opinion:
-      "Es una webapp que te permite ver el valor de las criptomonedas en tiempo real. Esta hecha con React y Styled-Components. La API que use es la de CryptoCompare.",
+      "Sigue el valor de las criptomonedas en tiempo real con esta webapp. Desarrollada con React y Styled-Components, utiliza la API de CryptoCompare para brindarte datos actualizados al instante.",
     tag: "REACT,STYLED-COMPONENTS,API,JAVASCRIPT",
     url: "https://cripto-app-tawny.vercel.app/",
     img: cryptoImage,
   },
   {
-    id: 1,
+    id: 2,
     title: "SISMOTRACK",
     opinion:
-      "Es una webapp que con un backend y un front end, se pueden ver los sismos ocurridos, el banckend esta hosteado en Render, esta con Node.js, y el frontend con React, Tailwindcss.",
+      "Visualiza la actividad sísmica en tiempo real con esta plataforma. Con un backend en Node.js y un frontend en React y TailwindCSS, permite consultar datos precisos sobre sismos recientes.",
     tag: "REACT,TAILWINDCSS,API,JAVASCRIPT,NODE.JS,VERCEL,RENDER",
     url: "https://sismo-app-chi.vercel.app/",
     img: sismoImage,
   },
   {
-    id: 2,
+    id: 3,
     title: "YaOficios",
     opinion:
-      "Es una webapp que te permite ver  y agregar personas con diferentes oficios, en esta vas a poder encontrar y filtrar por el oficio que necesitas, está pensada (por ahora) para la provincia de San Juan.",
+      "Encuentra y publica servicios de oficios fácilmente. Filtra por especialidad y contacta con profesionales en San Juan. Ideal para quienes buscan conectar oferta y demanda de trabajo.",
     tag: "REACT,TAILWINDCSS,MONGODB,JAVASCRIPT,CSS",
     url: "https://my-recommend-app.vercel.app/",
     img: YaOfiImgaImage,
   },
   {
-    id: 3,
+    id: 4,
     title: "Vet App",
     opinion:
-      "Es una webapp que te permite agregar Mascotas, junto al nombre del dueño, su correo y los sintomas, tambien te deja darle de alta e ir viendo gracias a localStorage cuantos pacientes has tenido.",
+      "Una herramienta esencial para el control de pacientes veterinarios. Permite registrar mascotas, dueños y síntomas, con un historial accesible gracias a LocalStorage.",
     tag: "REACT,TAILWINDCSS,JAVASCRIPT,LOCALSTORAGE",
     url: "https://my-calendar-app-ebon.vercel.app/",
     img: vetImage,
   },
   {
-    id: 4,
+    id: 5,
     title: "Snake Game",
-    opinion: "Es un simple juego emulando el clasico snake.",
-    tag: "HTML,CSS,JS,SCSS, CANVAS",
+    opinion:
+      "Revive el clásico Snake en esta versión moderna hecha con JavaScript y Canvas. Ágil, entretenido y perfecto para desafiar tu habilidad.",
+    tag: "HTML,CSS,JS,SCSS,CANVAS",
     url: "https://snake-js-taupe.vercel.app/",
     img: snakeImage,
   },
   {
-    id: 5,
+    id: 6,
     title: "Voicetext App",
     opinion:
-      "Es una web por la cual podes generar de voz a texto todo lo que digas.",
+      "Convierte tu voz en texto en tiempo real. Ideal para transcribir ideas, notas o mensajes de forma rápida y sencilla.",
     tag: "HTML,CSS,JS,SCSS",
     url: "https://voicetext.vercel.app/",
     img: voiceImage,
   },
   {
-    id: 6,
+    id: 7,
     title: "Courflix Netflix Clone",
-    opinion: "Es una web emulando la conocida plataforma.",
+    opinion:
+      "Un clon de Netflix desarrollado desde cero. Explora una interfaz similar a la plataforma original, con un diseño atractivo y responsive.",
     tag: "HTML,CSS,JS,SCSS",
     url: "https://courflix-vanilla-master.vercel.app/",
     img: courflixImage,
@@ -73,26 +84,33 @@ const projectsData = [
 function displayProjects() {
   const projectsContainer = document.querySelector(".section-content");
 
+  if (!projectsContainer) {
+    console.error("No se encontró el contenedor .section-content en el HTML.");
+    return;
+  }
+
   projectsData.forEach((project) => {
+    // Validar si project.tag existe antes de hacer split
     const tags = project.tag
-      .split(",")
-      .map(
-        (tag) => `<span class="section-content-images-tag ${tag}">${tag}</span>`
-      )
-      .join(" ");
+      ? project.tag
+          .split(",")
+          .map(
+            (tag) =>
+              `<span class="section-content-images-tag ${tag}">${tag}</span>`
+          )
+          .join(" ")
+      : ""; // Si no hay tags, usa una cadena vacía
 
     const projectElement = `
       <div class="section-content-images">
-        <div style="display: flex; flex-direction: column; align-items: center;">
-          <p class="section-content-images-title">${project.title}</p>
-          <a href="${project.url}" target="blank_">
-            <img src="${project.img}" alt="${project.title}" style="width: 100%; max-width: 100px; height: auto;" />
-          </a>
-          <p class="section-content-images-opinion">${project.opinion}</p>
-          <div>${tags}</div>
-        </div>
+        <p class="section-content-images-title">${project.title}</p>
+        <a href="${project.url}" target="blank_">
+          <img src="${project.img}" alt="${project.title}" style="width: 100px; height: auto;" />
+        </a>
+        <p class="section-content-images-opinion">${project.opinion}</p>
+        <div>${tags}</div>
       </div>
-        `;
+    `;
 
     projectsContainer.insertAdjacentHTML("beforeend", projectElement);
   });
